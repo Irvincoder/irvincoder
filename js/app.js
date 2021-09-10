5f27525106978dfbf6fbdd14acf3712e36490d0e
Irvin Gonzalez <alexdj19893@gmail.com>
2021-09-10 09:14:32 -0600

    Otros archivos

--- js/app.js
+++ js/app.js
@@ -1,19 +1,12 @@
-const body = document.querySelector("body");
-const navbar = document.querySelector(".navbar");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = () =>{
-      navbar.classList.add("show");
-      menuBtn.classList.add("hide");
-      body.classList.add("disabled");
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menu-open');
+    const NavbarToggle = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    NavbarToggle.classList.toggle('active');
 }
-
-cancelBtn.onclick = () =>{
-      body.classList.remove("disabled");
-      navbar.classList.remove("show");
-      menuBtn.classList.remove("hide");
-}
-
-window.onscroll = () =>{
-      this.scrollY > 20 ? navbar.classList.add("stiky") : navbar.classList.remove("sticky");
+function toggleClose(){
+    const toggleMenu = document.querySelector('.menu-close');
+    const NavbarToggle = document.querySelector('.navigation');
+    toggleMenu.classList.remove('active');
+    NavbarToggle.classList.remove('active');
 }
